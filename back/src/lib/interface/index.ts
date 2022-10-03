f6d7118c9ecb4ad02156e7b2af8f62980eec2da2
DL-Berkey <ruyria@naver.com>
2022-10-03 23:49:19 +0900

    [back] edit lib/interface/index.ts

--- back/src/lib/interface/index.ts
+++ back/src/lib/interface/index.ts
@@ -10,27 +10,6 @@ export interface UserInterface {
     comments?: Types.ObjectId;
 }
 
-export interface PostInterface {
-    postID: string;
-    author: Types.ObjectId;
-    anonymous: boolean;
-    title: string;
-    content: string;
-    commemts?: Types.ObjectId;
-    tags: Types.ObjectId;
-    expiration: string;
-}
-
-export interface CommentInterface {
-    author: Types.ObjectId;
-    content: string;
-}
-
-export interface TagInterface {
-    owner: Types.ObjectId;
-    name: string;
-}
-
 export interface Token {
     token: string;
     userID: string;
