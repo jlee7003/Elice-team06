2e4d242b4f92d4aeebcbb43537a23a3418d9d07b
DL-Berkey <ruyria@naver.com>
2022-10-07 14:37:10 +0900

    [FE/50] feat add put, delete methods in Api class

--- front/src/api/index.ts
+++ front/src/api/index.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+interface Data {
+    [key: string]: string;
+}
+
 class Api {
     private static instance: Api;
     private path: string;
@@ -22,11 +26,37 @@ class Api {
         return axios.get(url);
     }
 
-    async post(params: string[], data: { [key: string]: string }, accessToken: string) {
+    async post(params: string[], data: Data, accessToken: string) {
         const url = this.path + params.join("/");
 
         const bodyData = JSON.stringify(data);
         return axios.post(url, bodyData, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${accessToken}`,
+                refresh: sessionStorage.getItem("refreshToken") ?? "",
+            },
+        });
+    }
+
+    async put(params: string[], data: Data, accessToken: string) {
+        const url = this.path + params.join("/");
+
+        const bodyData = JSON.stringify(data);
+
+        return axios.put(url, bodyData, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${accessToken}`,
+                refresh: sessionStorage.getItem("refreshToken") ?? "",
+            },
+        });
+    }
+
+    async del(params: string[], accessToken: string) {
+        const url = this.path + params.join("/");
+
+        return axios.delete(url, {
             headers: {
                 "Content-Type": "application/json",
                 Authorization: `Bearer ${accessToken}`,
