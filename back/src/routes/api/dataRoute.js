7bf0219054babbd5133a2ab115b91c60dd18edb4
임지원 <9universe9th@gmail.com>
2022-10-10 12:54:03 +0900

    [BE/56] fix userService, userRoute

--- back/src/routes/api/dataRoute.js
+++ back/src/routes/api/dataRoute.js
@@ -6,7 +6,7 @@ const dataRoute = Router();
 dataRoute.get(
     "/data/sealevel",
     asyncHandler(async (req, res) => {
-        const result = await {"연간 해수면 데이터":0}; // Query | ORM으로 데이터 DB에서 import하는 코드
+        const result = await { "연간 해수면 데이터": 0 }; // Query | ORM으로 데이터 DB에서 import하는 코드
 
         res.send(result);
     })
@@ -15,7 +15,7 @@ dataRoute.get(
 dataRoute.get(
     "/data/emission",
     asyncHandler(async (req, res) => {
-        const result = await {"주요 3개국+전세계 및 한국 탄소 배출량 데이터":0};
+        const result = await { "주요 3개국+전세계 및 한국 탄소 배출량 데이터": 0 };
 
         res.send(result);
     })
@@ -24,7 +24,7 @@ dataRoute.get(
 dataRoute.get(
     "/data/temperture",
     asyncHandler(async (req, res) => {
-        const result = await {"주요 3개국+전세계 및 한국 온도 변화량 데이터":0};
+        const result = await { "주요 3개국+전세계 및 한국 온도 변화량 데이터": 0 };
 
         res.send(result);
     })
