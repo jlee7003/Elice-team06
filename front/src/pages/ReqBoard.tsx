6b7d490f56a65a4b2068e1853de580667fedc9ab
jpra2021 <jthvmcm2021@cuk.edu>
2022-10-11 15:58:58 +0900

    [FE/74] fix errors(not completed)

--- front/src/pages/ReqBoard.tsx
+++ front/src/pages/ReqBoard.tsx
@@ -1,6 +1,6 @@
 /*lib*/
 import { useRecoilState, useRecoilValue } from "recoil";
-import { useState } from "react";
+import { useState, useRef } from "react";
 /*data*/
 import userData from "../recoil/userData";
 /*styles*/
@@ -13,31 +13,50 @@ import {
     NavLink,
 } from "@/styles/pages/reqpage-style";
 /*boards*/
-import ReqeustCards from "@/components/RequestCard";
+import ReqeustCards from "@/components/RequestCards";
 import { postsSelector } from "@/recoil/requestPosts";
 import { useEffect } from "react";
+//pagination
+import Pagination from "@/components/pagination";
 
 const ReqPage = () => {
-    const [page, setPage] = useState(0);
-    const posts = useRecoilValue(postsSelector(page));
+    //     //page setting
+    //    //object[] 의 key가 page값이 된다. { 1: [{...}, {...}, {...}] }
+    //    //post... 로 받아온다고 치고...
+    //    const post:{
+    //     [key:number]:
+    //    }={ 1: [{...}, {...}, {...}] }
+    //    const totalPages=Object.keys(post).length;
+    //    const NavNum=Object.values(post).length;//페이지당 사이즈 넘버
+    //    const currentPage =useRef(1); //default page : 1
+    //    const startNum=useRef(1)
+    //    const [NavStartNum, setNavStartNum]=useState(1);
+    //    const [NavEndNum, setNavEndNum]=useState(5);
 
-    useEffect(() => {
-        console.log("pageATOM : ", page);
-    }, [page]);
+    //    //useState를 사용하여 startNum과 endNum을 세팅
+    //    const firstPage=(currentPage:number)=>{
+    //     setNavStartNum(currentPage-(currentPage%NavNum)+1);
+    //    }
+    //    const endPage=(currentPage:number)=>{
+    //     setNavEndNum(currentPage-(currentPage%NavNum)+NavNum);
+    //    }
 
-    useEffect(() => {
-        setPage(1);
-    }, []);
+    //    //이걸 가지고 이제 뿌려주는거
+    //    //nav에서 12345 얘네를 각각 클릭하면 currnet페이지 핸들링하기
+    //    //curr를 compo내부로 .......... 리랜더링 할필요가 없는 값인데 페이지를 누를때마다 리랜더링이? 되네?
 
-    useEffect(() => {
-        console.log("posts : ", posts);
-    }, [posts]);
+    //    // 1 2 3 4 5 현재 페이지가 바뀌는 것도 알아야 한다->useRef
+    //    const handlepage=(name:number)=>{
+    //     return currentPage.current+=name
+    //    }
 
-    const handlePageUp = () => {
-        setPage((oldPage) => {
-            return oldPage + 1;
-        });
-    };
+    //    const ChangeNextBlock=(currentPage:number)=>{
+
+    //     return startNum.current=(currentPage-(currentPage%NavNum)+1)+5
+
+    //    }
+
+    //클릭 시 currentpage up해줄 함수
 
     return (
         <div>
@@ -94,3 +113,24 @@ export default ReqPage;
 //         </div>
 //     </>
 // );
+
+// const [page, setPage] = useState(0);
+//     const posts = useRecoilValue(postsSelector(page));
+
+//     useEffect(() => {
+//         console.log("pageATOM : ", page);
+//     }, [page]);
+
+//     useEffect(() => {
+//         setPage(1);
+//     }, []);
+
+//     useEffect(() => {
+//         console.log("posts : ", posts);
+//     }, [posts]);
+
+//     const handlePageUp = () => {
+//         setPage((oldPage) => {
+//             return oldPage + 1;
+//         });
+//     };
