1ca65b77ac6905d2837cad7b24c32613ba4268c6
KongTi <hjm0137@naver.com>
2022-10-12 17:16:12 +0900

    [BE/61]fix challengeAPI

--- back/src/services/challengeService.js
+++ back/src/services/challengeService.js
@@ -5,25 +5,76 @@ const prisma = new PrismaClient();
 // the abbreviation for challenge: chl
 class challengeService {
     static async getchls() {
-        const challenges = await prisma.Challenge.findMany();
+        const challenges = await prisma.Challenge.findMany({
+            select: {
+                id: true,
+                title: true,
+                start_date: true,
+                due_date: true,
+                _count: { select: { Challenger: true } },
+            },
+        });
         await prisma.$disconnect();
         return challenges;
     }
 
-    static async findchl({ user_email, challengeId = null }) {
+    static async findchl({ nickname, challengeId = null }) {
         let findChallenge;
 
         if (challengeId) {
             findChallenge = await prisma.Challenge.findUniqueOrThrow({
                 where: { id: Number(challengeId) },
+                include: { Challenger: { select: { nickname: true } } },
             });
         } else {
-            findChallenge = await prisma.Challenge.findMany({
-                where: { proposer: user_email },
+            findChallenge = await prisma.Challenger.findMany({
+                where: { nickname },
+                select: {
+                    challenge_id: true,
+                    Challenge: {
+                        select: {
+                            title: true,
+                            start_date: true,
+                            due_date: true,
+                            _count: { select: { Challenger: true } },
+                        },
+                    },
+                },
             });
         }
         await prisma.$disconnect();
         return findChallenge;
+    }
+
+    static async joinchl({ nickname, challengeId }) {
+        let joinChallenge = await prisma.Challenger.findUnique({
+            where: {
+                challenge_id_nickname: {
+                    challenge_id: Number(challengeId),
+                    nickname,
+                },
+            },
+        });
+
+        if (!joinChallenge) {
+            joinChallenge = await prisma.Challenger.create({
+                data: { challenge_id: Number(challengeId), nickname },
+            });
+            await prisma.$disconnect();
+            return joinChallenge;
+        }
+
+        await prisma.Challenger.delete({
+            where: {
+                challenge_id_nickname: {
+                    challenge_id: Number(challengeId),
+                    nickname,
+                },
+            },
+        });
+
+        await prisma.$disconnect();
+        return "cancel";
     }
 
     static async addchl(input) {
@@ -50,7 +101,8 @@ class challengeService {
         return updateChallenge;
     }
 
-    static async deletechl(challengeId = null) {
+    static async deletechl(challengeId) {
+        await prisma.Challenger.deleteMany({ where: { challenge_id: Number(challengeId) } });
         const deleteChallenge = await prisma.Challenge.delete({
             where: { id: Number(challengeId) },
         });
