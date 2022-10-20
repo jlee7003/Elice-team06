92f5314b1ebeddf70ff7cabe5996f83a3d124e2d
jiyun <fbwldbs93@naver.com>
2022-10-21 02:29:29 +0900

    [FE/110] final.. - admin desgin

--- front/src/styles/pages/admin-style.ts
+++ front/src/styles/pages/admin-style.ts
@@ -23,7 +23,9 @@ export const AdminBoxHeader = styled.div`
     position: relative;
 
     width: 100%;
-    height: 30%;
+    /* height: 30%; */
+    padding-bottom: 80px;
+    font-weight: 600;
 `;
 
 export const AdmintBoxNav = styled.div`
@@ -35,8 +37,12 @@ export const AdmintBoxNav = styled.div`
 
 export const AdminBoxNavButton = styled.button`
     color: #000;
-    font-size: ${cssUnit.fontSize.medium};
-
+    font-size: 20px;
+    background-color: #fff;
+    border: 1px solid #000;
+    padding: 15px 30px;
+    box-sizing: border-box;
+    border-radius: 20px;
     & + & {
         margin-left: 30px;
     }
@@ -54,8 +60,30 @@ export const AdminButton = styled(AdminTitle)`
     position: absolute;
     top: 10px;
     right: 10px;
+    background-color: #61be92;
+    font-size: 20px;
+    color: #fff;
+    height: 50px;
+    line-height: 50px;
+    padding: 0 20px;
+    border-radius: 20px;
+    cursor: pointer;
+    i {
+        vertical-align: -3px;
+        padding-right: 5px;
+    }
+`;
 
-    font-size: ${cssUnit.fontSize.medium};
+export const DataWrap = styled.div`
+    width: 100%;
+    border: 1px solid #eee;
+    background-color: #f0f0f0;
+    padding: 20px;
+    box-sizing: border-box;
+    border-radius: 20px;
+    height: 700px;
+    overflow-y: auto;
+    margin-top: 50px;
 `;
 
 export const DataBox = styled.div`
@@ -63,6 +91,16 @@ export const DataBox = styled.div`
     height: fit-content;
 
     overflow-y: scroll;
+    border-bottom: 1px solid #d7d7d7;
+    padding: 10px 0;
+    margin: 10px 0;
+    background-color: #fff;
+    text-indent: 20px;
+    border-radius: 8px;
+
+    &:first-child {
+        margin-top: 0;
+    }
 `;
 
 export const DataLow = styled.span`
