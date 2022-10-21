88ffea44749efa032d4a33f4ba1a0997ff2d20b9
jlee7003 <jlee7003@naver.com>
2022-10-21 19:56:17 +0900

    [FE] fixed img

--- front/src/styles/pages/signup-style.ts
+++ front/src/styles/pages/signup-style.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 import cssUnit from "@/lib/cssUnit";
 import assets from "@/lib/assets";
-
-const bannerImg = assets("banner_img.jpg");
+import bannerImg from "@/assets/banner_img.jpg";
+// const bannerImg = assets("banner_img.jpg");
 export const Container = styled.div`
     display: grid;
     place-items: center;
